d8f74d788199de9cec5d5830a38291773e0a9465
CryptXDaVincis <robbypaol@gmail.com>
2025-03-19 07:18:46 +0700

    Update wrapper.blade.php

--- :dashboard/wrapper.blade.php
+++ :dashboard/wrapper.blade.php
@@ -1,5 +1,5 @@
 <style>
-  /* customizable snowflake styling */
+  /* Gaya elemen angka dan rumus matematika */
   .snowflake {
     color: #fff;
     font-size: 1em;
@@ -7,154 +7,65 @@
     text-shadow: 0 0 5px #000;
   }
 
-  .snowflake,.snowflake .inner{
-    animation-iteration-count:infinite;
-    animation-play-state:running
+  .snowflake, .snowflake .inner {
+    animation-iteration-count: infinite;
+    animation-play-state: running;
   }
-  @keyframes snowflakes-fall{
-    0%{
-      transform:translateY(0)
-    }
-    100%{
-      transform:translateY(110vh)
-    }
-  }
-  @keyframes snowflakes-shake{
-    0%,100%{
-      transform:translateX(0)
-    }
-    50%{
-      transform:translateX(80px)
-    }
-  }
-  .snowflake{
-    position:fixed;
-    top:-10%;
-    -webkit-user-select:none;
-    user-select:none;
-    cursor:default;
-    animation-name:snowflakes-shake;
-    animation-duration:3s;
-    animation-timing-function:ease-in-out
-  }
-  .snowflake .inner{
-    animation-duration:10s;
-    animation-name:snowflakes-fall;
-    animation-timing-function:linear
-  }
-  .snowflake:nth-of-type(0){
-    left:1%;
-    animation-delay:0s
-  }
-  .snowflake:nth-of-type(0) .inner{
-    animation-delay:0s
-  }
-  .snowflake:first-of-type{
-    left:10%;
-    animation-delay:1s
-  }
-  .snowflake:first-of-type .inner,.snowflake:nth-of-type(8) .inner{
-    animation-delay:1s
-  }
-  .snowflake:nth-of-type(2){
-    left:20%;
-    animation-delay:.5s
-  }
-  .snowflake:nth-of-type(2) .inner,.snowflake:nth-of-type(6) .inner{
-    animation-delay:6s
-  }
-  .snowflake:nth-of-type(3){
-    left:30%;
-    animation-delay:2s
-  }
-  .snowflake:nth-of-type(11) .inner,.snowflake:nth-of-type(3) .inner{
-    animation-delay:4s
-  }
-  .snowflake:nth-of-type(4){
-    left:40%;
-    animation-delay:2s
-  }
-  .snowflake:nth-of-type(10) .inner,.snowflake:nth-of-type(4) .inner{
-    animation-delay:2s
-  }
-  .snowflake:nth-of-type(5){
-    left:50%;
-    animation-delay:3s
-  }
-  .snowflake:nth-of-type(5) .inner{
-    animation-delay:8s
-  }
-  .snowflake:nth-of-type(6){
-    left:60%;
-    animation-delay:2s
-  }
-  .snowflake:nth-of-type(7){
-    left:70%;
-    animation-delay:1s
-  }
-  .snowflake:nth-of-type(7) .inner{
-    animation-delay:2.5s
-  }
-  .snowflake:nth-of-type(8){
-    left:80%;
-    animation-delay:0s
-  }
-  .snowflake:nth-of-type(9){
-    left:90%;
-    animation-delay:1.5s
-  }
-  .snowflake:nth-of-type(9) .inner{
-    animation-delay:3s
-  }
-  .snowflake:nth-of-type(10){
-    left:25%;
-    animation-delay:0s
-  }
-  .snowflake:nth-of-type(11){
-    left:65%;
-    animation-delay:2.5s
-  }
-</style>
-<div class="snowflakes" aria-hidden="true">
-  <div class="snowflake">
-    <div class="inner">●</div>
-  </div>
-  <div class="snowflake">
-    <div class="inner">●</div>
-  </div>
-  <div class="snowflake">
-    <div class="inner">●</div>
-  </div>
-  <div class="snowflake">
-    <div class="inner">●</div>
-  </div>
-  <div class="snowflake">
-    <div class="inner">●</div>
-  </div>
-  <div class="snowflake">
-    <div class="inner">●</div>
-  </div>
-  <div class="snowflake">
-    <div class="inner">●</div>
-  </div>
-  <div class="snowflake">
-    <div class="inner">●</div>
-  </div>
-  <div class="snowflake">
-    <div class="inner">●</div>
-  </div>
-  <div class="snowflake">
-    <div class="inner">●</div>
-  </div>
-  <div class="snowflake">
-    <div class="inner">●</div>
-  </div>
-  <div class="snowflake">
-    <div class="inner">●</div>
-  </div>
-</div>
-<img src="/extensions/snowflakes/snow.png" class="snow">
 
+  @keyframes snowflakes-fall {
+    0% { transform: translateY(0); }
+    100% { transform: translateY(110vh); }
+  }
+
+  @keyframes snowflakes-shake {
+    0%, 100% { transform: translateX(0); }
+    50% { transform: translateX(80px); }
+  }
+
+  .snowflake {
+    position: fixed;
+    top: -10%;
+    -webkit-user-select: none;
+    user-select: none;
+    cursor: default;
+    animation-name: snowflakes-shake;
+    animation-duration: 3s;
+    animation-timing-function: ease-in-out;
+  }
+
+  .snowflake .inner {
+    animation-duration: 10s;
+    animation-name: snowflakes-fall;
+    animation-timing-function: linear;
+  }
+
+  /* Posisi awal dan animasi delay untuk variasi */
+  .snowflake:nth-of-type(1) { left: 5%; animation-delay: 0s; }
+  .snowflake:nth-of-type(2) { left: 15%; animation-delay: 1s; }
+  .snowflake:nth-of-type(3) { left: 25%; animation-delay: 2s; }
+  .snowflake:nth-of-type(4) { left: 35%; animation-delay: 3s; }
+  .snowflake:nth-of-type(5) { left: 45%; animation-delay: 4s; }
+  .snowflake:nth-of-type(6) { left: 55%; animation-delay: 2s; }
+  .snowflake:nth-of-type(7) { left: 65%; animation-delay: 1s; }
+  .snowflake:nth-of-type(8) { left: 75%; animation-delay: 3s; }
+  .snowflake:nth-of-type(9) { left: 85%; animation-delay: 5s; }
+  .snowflake:nth-of-type(10) { left: 95%; animation-delay: 4s; }
+</style>
+
+<div class="snowflakes" aria-hidden="true">
+  <div class="snowflake"><div class="inner">E=mc²</div></div>
+  <div class="snowflake"><div class="inner">π ≈ 3.14</div></div>
+  <div class="snowflake"><div class="inner">9.81 m/s²</div></div>
+  <div class="snowflake"><div class="inner">F=ma</div></div>
+  <div class="snowflake"><div class="inner">√2 ≈ 1.41</div></div>
+  <div class="snowflake"><div class="inner">c = 299,792,458 m/s</div></div>
+  <div class="snowflake"><div class="inner">P = IV</div></div>
+  <div class="snowflake"><div class="inner">H₂O</div></div>
+  <div class="snowflake"><div class="inner">e = 2.718</div></div>
+  <div class="snowflake"><div class="inner">Ω = V/I</div></div>
+</div>
+
+<img src="/extensions/snowflakes/snow.png" class="snow">
 
 <style id="snowflakes-extension">
   #app {
